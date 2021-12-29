dbe751b0a08169e2be335fb0d8d092ab34482c78
WitoldRadzik04 <witoldradzik.2004@gmail.com>
2021-12-29 12:13:02 +0100

    displays machine WORKS

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os # syntax: os.system("cmd do something")
 import pygetwindow as gw
 import time
 path_this = os.path.dirname(__file__)
-
+key = "]dkafsjfgpioawehgji=-0]"
 def tabs_setup_machine():
     pass
 
@@ -13,7 +13,17 @@ def displays_setup_machine():
         display_number = data["meta"][0]["display_number"]
 
         for i in range(display_number):
-            os.system("start "+path_this+f"\movable.bat {i+1}")
+            os.system(f'start "{key}{i+1}" '+path_this+f'\movable.bat {i+1}')
+
+    input("Move windows 1 - 3 to their correspoding displays' centers, then focus this window and click enter ")
+    for i in range(display_number):
+        current_movable = gw.getWindowsWithTitle(key+str(i+1))[0] 
+        data["displays"][i]["x_coord"] = current_movable.left
+        data["displays"][i]["y_coord"] = current_movable.top
+
+    with open(os.path.join(path_this, "setup.json"), 'w') as json_file: 
+        json.dump(data, json_file)
+
 
 def main_thread():
     task = input()
