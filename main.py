4488fb62cfe20897284a90e4a407fe133228907d
WitoldRadzik04 <witoldradzik.2004@gmail.com>
2021-12-29 09:17:54 +0100

    New main works, new json

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+import os # syntax: os.system("cmd do something")
+import pygetwindow as gw
+import time
+
+with open('setup.json', 'r') as json_file:
+    data = json.load(json_file)
+    display_number = data["meta"][0]["display_number"]
+    displays = data["displays"]
+    tabs = data["tabs"]
+
+os.system("taskkill /F /IM discord.exe")
+for i in range(display_number):
+    basic_batch = "start chrome.exe --new-window "
+    for shit in tabs[i]:
+        basic_batch += shit + " "
+    os.system(basic_batch)
+    time.sleep(0.5)
+    active_window = gw.getActiveWindow()
+    active_window.restore()
+    active_window.moveTo(displays[i]["x_coord"], displays[i]["y_coord"])
+    active_window.maximize()
